2d34cdc9c03998c589edd25da517b93c57d46f71
SimonMTS <info@simonstriekwold.nl>
2020-10-02 19:46:39 +0200

    added project structure

---
+++ Krul/Krul/Krul.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "../../libcurl_git/curl/builds/libcurl-vc-x86-release-static-ipv6-sspi-schannel/include/curl/curl.h"
+
+int main(int argc, char* argv[])
+{
+    CURL* req = curl_easy_init();
+    CURLcode res;
+    if (req)
+    {
+        curl_easy_setopt(req, CURLOPT_URL, "www.google.com");
+        curl_easy_setopt(req, CURLOPT_FOLLOWLOCATION, 1L);
+        res = curl_easy_perform(req);
+        if (res != CURLE_OK)
+        {
+            fprintf(stderr, "curl_easy_operation() failed : %s\n", curl_easy_strerror(res));
+        }
+    }
+    curl_easy_cleanup(req);
+    return 0;
+}
